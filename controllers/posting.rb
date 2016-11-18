430bbb8693a6cd11ab5c0ef44afccdb7ec9eb3c5
Soumya Ray <soumya.ray@gmail.com>
2016-11-18 10:41:12 +0800

    Add dry-transaction require in correct init.rb file

--- controllers/posting.rb
+++ controllers/posting.rb
@@ -1,5 +1,4 @@
 # frozen_string_literal: true
-require 'dry-transaction'
 
 # Posting routes
 class FaceGroupAPI < Sinatra::Base
--- services/init.rb
+++ services/init.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 require 'dry-monads'
 require 'dry-container'
+require 'dry-transaction'
 require 'facegroup'
 
 Dir.glob("#{File.dirname(__FILE__)}/*.rb").each do |file|
